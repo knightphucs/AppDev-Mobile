fba5b96f0e4b788480cb8123f12c380ca9207dec
knightphucs <phuchoang3103@gmail.com>
2025-10-01 11:20:37 +0700

    Add dish detail component

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import { FlatList, View } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
+import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDish: null
+    };
+  }
+
   render() {
     return (
-      <FlatList data={this.props.dishes}
-        renderItem={({ item, index }) => this.renderMenuItem(item, index)}
-        keyExtractor={(item) => item.id.toString()} />
+      <View style={{ flex: 1 }}>
+        <FlatList data={this.props.dishes}
+          renderItem={({ item, index }) => this.renderMenuItem(item, index)}
+          keyExtractor={(item) => item.id.toString()} />
+        <Dishdetail dish={this.state.selectedDish} />
+      </View>
     );
   }
+
   renderMenuItem(item, index) {
     return (
-      <ListItem>
+      <ListItem key={index} onPress={() => this.onDishSelect(item)}>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
           <Avatar source={require('./images/uthappizza.png')} />
           <ListItem.Content style={{ marginLeft: 10 }}>
@@ -22,6 +34,9 @@ class Menu extends Component {
         </View>
       </ListItem>
     );
+  }
+  onDishSelect(item) {
+    this.setState({ selectedDish: item });
   }
 }
 export default Menu;
